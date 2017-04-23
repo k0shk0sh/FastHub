6614ee57c24cc89036c8ff871688abd837cea907
Kosh <kosh20111@gmail.com>
2017-04-24 02:31:13 +0800

    this commit adds russian language, fixes #348 and fixes #286

--- app/src/main/java/com/fastaccess/ui/modules/notification/NotificationsPresenter.java
+++ app/src/main/java/com/fastaccess/ui/modules/notification/NotificationsPresenter.java
@@ -33,10 +33,7 @@ public class NotificationsPresenter extends BasePresenter<NotificationsMvp.View>
             Notification item = model.getNotification();
             if (v.getId() == R.id.markAsRead) {
                 if (item.isUnread() && !PrefGetter.isMarkAsReadEnabled()) {
-                    item.setUnread(false);
-                    manageSubscription(item.save(item).subscribe());
-                    sendToView(view -> view.onUpdateReadState(new GroupedNotificationModel(item), position));
-                    ReadNotificationService.start(v.getContext(), item.getId());
+                    markAsRead(position, v, item);
                 }
             } else if (v.getId() == R.id.unsubsribe) {
                 item.setUnread(false);
@@ -45,6 +42,9 @@ public class NotificationsPresenter extends BasePresenter<NotificationsMvp.View>
                 ReadNotificationService.unSubscribe(v.getContext(), item.getId());
             } else {
                 if (item.getSubject() != null && item.getSubject().getUrl() != null) {
+                    if (item.isUnread() && !PrefGetter.isMarkAsReadEnabled()) {
+                        markAsRead(position, v, item);
+                    }
                     if (getView() != null) getView().onClick(item.getSubject().getUrl());
                 }
             }
@@ -53,6 +53,13 @@ public class NotificationsPresenter extends BasePresenter<NotificationsMvp.View>
             if (repo == null) return;
             getView().onMarkAllByRepo(repo);
         }
+    }
+
+    private void markAsRead(int position, View v, Notification item) {
+        item.setUnread(false);
+        manageSubscription(item.save(item).subscribe());
+        sendToView(view -> view.onUpdateReadState(new GroupedNotificationModel(item), position));
+        ReadNotificationService.start(v.getContext(), item.getId());
     }
 
     @Override public void onItemLongClick(int position, View v, GroupedNotificationModel item) {}
--- language_contributors.md
+++ language_contributors.md
@@ -4,3 +4,4 @@
 - German [@failex234](https://github.com/failex234)
 - Chinese [@Devifish](https://github.com/Devifish)
 - Indonesian [@dikiaap](https://github.com/dikiaap)
+- Russian [@dedepete](https://github.com/dedepete)
