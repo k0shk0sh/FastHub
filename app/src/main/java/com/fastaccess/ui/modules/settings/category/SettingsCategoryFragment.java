ee6d21fa5f521a3177db94290fecb274f32c2901
Jedi Burrell <JediBurrell@gmail.com>
2017-05-16 07:28:15 -0400

    Add Backup Settings. - #482
    You can now back up and restore your settings, this is an appeal to
    Issue #482.

--- app/src/main/java/com/fastaccess/ui/modules/settings/category/SettingsCategoryFragment.java
+++ app/src/main/java/com/fastaccess/ui/modules/settings/category/SettingsCategoryFragment.java
@@ -1,29 +1,60 @@
 package com.fastaccess.ui.modules.settings.category;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.util.Log;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.fastaccess.BuildConfig;
 import com.fastaccess.R;
 import com.fastaccess.helper.ActivityHelper;
+import com.fastaccess.helper.FileHelper;
+import com.fastaccess.helper.InputHelper;
 import com.fastaccess.helper.PrefGetter;
 import com.fastaccess.helper.PrefHelper;
 import com.fastaccess.provider.tasks.notification.NotificationSchedulerJobTask;
 import com.fastaccess.ui.base.mvp.BaseMvp;
 import com.fastaccess.ui.modules.changelog.ChangelogBottomSheetDialog;
 import com.fastaccess.ui.widgets.SpannableBuilder;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.Set;
 
 import butterknife.BindView;
 import es.dmoral.toasty.Toasty;
+
+import static android.app.Activity.RESULT_OK;
 
 public class SettingsCategoryFragment extends PreferenceFragmentCompat implements Preference.OnPreferenceChangeListener {
 
     @BindView(R.id.settingsContainer)
     FrameLayout settingsContainer;
+
+    private static int PERMISSION_REQUEST_CODE = 128;
+    private static int RESTORE_REQUEST_CODE = 256;
 
     private BaseMvp.FAView callback;
     private String appTheme;
@@ -92,6 +123,59 @@ public class SettingsCategoryFragment extends PreferenceFragmentCompat implement
                         .bold(BuildConfig.VERSION_NAME)
                         .append(")"));
                 break;
+            case 4:
+                addPreferencesFromResource(R.xml.backup_settings);
+                findPreference("backup").setOnPreferenceClickListener((Preference preference) -> {
+
+
+                    if (ContextCompat.checkSelfPermission(getActivity(),
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        Map<String, ?> settings_ = PrefHelper.getAll();
+                        settings_.remove("token");
+                        String json = new Gson().toJson(settings_);
+                        String path =
+                                Environment.getExternalStorageDirectory() + File.separator  + "FastHub";
+                        File folder = new File(path);
+                        folder.mkdirs();
+                        File backup = new File(folder, "backup.json");
+
+                        try {
+                            backup.createNewFile();
+                            FileOutputStream outputStream = new FileOutputStream(backup);
+                            OutputStreamWriter myOutWriter = new OutputStreamWriter(outputStream);
+                            myOutWriter.append(json);
+
+                            myOutWriter.close();
+
+                            outputStream.flush();
+                            outputStream.close();
+                        }
+                        catch (IOException e) {
+                            Log.e(getTag(), "Couldn't backup: " + e.toString());
+                        }
+
+                        PrefHelper.set("backed_up", new SimpleDateFormat("MM/dd").format(new Date()));
+                    } else {
+                        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+                    }
+
+                    return true;
+                });
+                if(PrefHelper.getString("backed_up")!=null)
+                    findPreference("backup").setSummary(getString(R.string.backup_summary, PrefHelper.getString("backed_up")));
+                else
+                    findPreference("backup").setSummary("");
+                findPreference("restore").setOnPreferenceClickListener(preference -> {
+                    if (ContextCompat.checkSelfPermission(getActivity(),
+                            Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        showFileChooser();
+                    } else {
+                        requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+                    }
+
+                    return true;
+                });
+                break;
             default:
                 addPreferencesFromResource(R.xml.fasthub_settings);
                 break;
@@ -148,6 +232,97 @@ public class SettingsCategoryFragment extends PreferenceFragmentCompat implement
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode==PERMISSION_REQUEST_CODE) {
+            if (permissions[0].equals(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Map<String, ?> settings = PrefHelper.getAll();
+                    settings.remove("token");
+                    String json = new Gson().toJson(settings);
+                    String path =
+                            Environment.getExternalStorageDirectory() + File.separator  + "FastHub";
+                    File folder = new File(path);
+                    folder.mkdirs();
+                    File backup = new File(folder, "backup.json");
+
+                    try {
+                        backup.createNewFile();
+                        FileOutputStream outputStream = new FileOutputStream(backup);
+                        OutputStreamWriter myOutWriter = new OutputStreamWriter(outputStream);
+                        myOutWriter.append(json);
+
+                        myOutWriter.close();
+
+                        outputStream.flush();
+                        outputStream.close();
+                    }
+                    catch (IOException e) {
+                        Log.e(getTag(), "Couldn't backup: " + e.toString());
+                    }
+
+                    PrefHelper.set("backed_up", new SimpleDateFormat("MM/dd").format(new Date()));
+                    findPreference("backup").setSummary(getString(R.string.backup_summary, PrefHelper.getString("backed_up")));
+                } else {
+                    Toasty.error(getContext(), getString(R.string.permission_failed)).show();
+                }
+            } else if (permissions[0].equals(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    showFileChooser();
+                } else {
+                    Toasty.error(getContext(), getString(R.string.permission_failed)).show();
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode==RESTORE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                StringBuilder json = new StringBuilder();
+                try {
+                    InputStream inputStream = getContext().getContentResolver().openInputStream(data.getData());
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(
+                            inputStream));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        json.append(line);
+                    }
+                    reader.close();
+                    inputStream.close();
+                }catch (IOException e) {
+                    Toasty.error(getContext(), getString(R.string.error)).show();
+                }
+
+                Gson gson = new Gson();
+                JsonObject jsonObject = gson.fromJson(json.toString(), JsonObject.class);
+                Set<Map.Entry<String, JsonElement>> entrySet = jsonObject.entrySet();
+                for(Map.Entry<String,JsonElement> entry : entrySet){
+                    if (entry.getValue().getAsJsonPrimitive().isBoolean())
+                        PrefHelper.set(entry.getKey(), entry.getValue().getAsBoolean());
+                    else if (entry.getValue().getAsJsonPrimitive().isNumber())
+                        PrefHelper.set(entry.getKey(), entry.getValue().getAsNumber().intValue());
+                    else if (entry.getValue().getAsJsonPrimitive().isString())
+                        PrefHelper.set(entry.getKey(), entry.getValue().getAsString());
+                    PrefHelper.set(entry.getKey(), entry.getValue());
+                    Log.d(getTag(), entry.getKey() + ": " + entry.getValue());
+                }
+                callback.onThemeChanged();
+            }
+        }
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("application/json");
+
+        startActivityForResult(Intent.createChooser(intent, getString(R.string.select_backup)), RESTORE_REQUEST_CODE);
     }
 
 }
