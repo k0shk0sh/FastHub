81dd458f69903dbbb8b3284a90002ce6f334dfe9
Kosh <kosh20111@gmail.com>
2017-05-28 18:41:48 +0800

    reverted back from Android studio 3.0 and released 2.5.3

--- app/src/main/java/com/fastaccess/helper/PrefGetter.java
+++ app/src/main/java/com/fastaccess/helper/PrefGetter.java
@@ -171,9 +171,11 @@ public class PrefGetter {
     }
 
     public static int getNotificationTaskDuration() {
-        String prefValue = PrefHelper.getString("notificationTime");
-        if (prefValue != null) {
-            return notificationDurationMillis(prefValue);
+        if (PrefHelper.isExist("notificationEnabled") && PrefHelper.getBoolean("notificationEnabled")) {
+            String prefValue = PrefHelper.getString("notificationTime");
+            if (prefValue != null) {
+                return notificationDurationMillis(prefValue);
+            }
         }
         return -1;
     }
