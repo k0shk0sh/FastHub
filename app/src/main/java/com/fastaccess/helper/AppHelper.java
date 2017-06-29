114d6eae1868038f34f8f25413cd6b9cfa86eac8
Yakov <yakov116@gmail.com>
2017-06-29 18:04:08 -0400

    detect emulator for signiture editing

--- app/src/main/java/com/fastaccess/helper/AppHelper.java
+++ app/src/main/java/com/fastaccess/helper/AppHelper.java
@@ -124,9 +124,21 @@ public class AppHelper {
         String model = Build.MODEL;
         if (model.startsWith(manufacturer)) {
             return InputHelper.capitalizeFirstLetter(model);
+        } else if (isEmulator()){
+            return "Android Emulator";
         } else {
             return InputHelper.capitalizeFirstLetter(manufacturer) + " " + model;
         }
     }
 
+    private static boolean isEmulator() {
+        return Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.startsWith("unknown")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDK built for x86")
+                || Build.MANUFACTURER.contains("Genymotion")
+                || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+                || "google_sdk".equals(Build.PRODUCT);
+    }
 }
