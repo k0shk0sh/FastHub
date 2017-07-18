bde1ef18d3aa092528081fe2f35038be608eb12a
kosh <kosh20111@gmail.com>
2017-07-18 12:30:06 +0800

    fixed empty notifications (tagging 4.0.2)

--- app/src/main/java/com/fastaccess/data/dao/model/AbstractNotification.java
+++ app/src/main/java/com/fastaccess/data/dao/model/AbstractNotification.java
@@ -13,6 +13,7 @@ import com.fastaccess.helper.RxHelper;
 import java.util.Date;
 import java.util.List;
 
+import io.reactivex.Observable;
 import io.reactivex.Single;
 import io.reactivex.disposables.Disposable;
 import io.requery.BlockingEntityStore;
@@ -71,9 +72,9 @@ import lombok.NoArgsConstructor;
         })).subscribe(o -> {/*do nothing*/}, Throwable::printStackTrace);
     }
 
-    @android.support.annotation.Nullable public static Disposable save(@android.support.annotation.Nullable List<Notification> models) {
+    public static Disposable save(@android.support.annotation.Nullable List<Notification> models) {
         if (models == null || models.isEmpty()) {
-            return null;
+            return Observable.empty().subscribe();
         }
         return RxHelper.getSingle(Single.fromPublisher(s -> {
             try {
