967cce5969a32c171cf221d82e7bf93d39cafa87
Yakov <yakov116@gmail.com>
2017-07-24 02:20:35 -0400

    Fixed extra space

--- app/src/main/java/com/fastaccess/helper/AppHelper.java
+++ app/src/main/java/com/fastaccess/helper/AppHelper.java
@@ -80,11 +80,12 @@ public class AppHelper {
         }
         builder.append("- **Model:** ")
                 .append(model)
-                .append("  \n").append("---").append("\n\n");
+                .append("  \n").append("---").append("\n");
         if (!Locale.getDefault().getLanguage().equals(new Locale("en").getLanguage())) {
             builder.append("<--")
                     .append(App.getInstance().getString(R.string.english_please))
-                    .append("-->");
+                    .append("-->")
+                    .append("\n");
         }
         return builder.toString();
     }
