d90f19c3f8103ddd17b1094901c09ce531fa95e3
Yakov <yakov116@gmail.com>
2017-08-02 10:24:37 -0400

    Quick fix

--- app/src/main/java/com/fastaccess/helper/AppHelper.java
+++ app/src/main/java/com/fastaccess/helper/AppHelper.java
@@ -73,14 +73,16 @@ public class AppHelper {
                 .append("**Android Version: ").append(String.valueOf(Build.VERSION.RELEASE)).append(" (SDK: ")
                 .append(String.valueOf(Build.VERSION.SDK_INT)).append(")**").append("  \n")
                 .append("**Device Information:**").append("  \n")
-                .append("- **" + (!model.equalsIgnoreCase(brand) ? "Manufacturer" : "Manufacturer&Brand") + ":** ").append(Build.MANUFACTURER)
+                .append("- **")
+                .append(!model.equalsIgnoreCase(brand) ? "Manufacturer" : "Manufacturer&Brand")
+                .append(":** ")
+                .append(Build.MANUFACTURER)
                 .append("  \n");
         if (!(model.equalsIgnoreCase(brand) || "google".equals(Build.BRAND))) {
             builder.append("- **Brand:** ").append(brand).append("  \n");
         }
-        builder.append("- **Model:** ")
-                .append(model)
-                .append("  \n").append("---").append("\n");
+        builder.append("- **Model:** ").append(model).append("  \n")
+                .append("---").append("\n\n");
         if (!Locale.getDefault().getLanguage().equals(new Locale("en").getLanguage())) {
             builder.append("<--")
                     .append(App.getInstance().getString(R.string.english_please))
