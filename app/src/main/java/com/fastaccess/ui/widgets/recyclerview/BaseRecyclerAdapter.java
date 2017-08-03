92812438137cc04082092ee6fd0320c2fc947013
kosh <kosh20111@gmail.com>
2017-08-03 11:38:34 +0800

    fixed footer span lookup.

--- app/src/main/java/com/fastaccess/ui/widgets/recyclerview/BaseRecyclerAdapter.java
+++ app/src/main/java/com/fastaccess/ui/widgets/recyclerview/BaseRecyclerAdapter.java
@@ -223,9 +223,10 @@ public abstract class BaseRecyclerAdapter<M, VH extends BaseViewHolder,
     private void addSpanLookup(ViewGroup parent) {
         if (parent instanceof RecyclerView) {
             if (((RecyclerView) parent).getLayoutManager() instanceof GridLayoutManager) {
-                ((GridLayoutManager) ((RecyclerView) parent).getLayoutManager()).setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+                GridLayoutManager layoutManager = ((GridLayoutManager) ((RecyclerView) parent).getLayoutManager());
+                layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
                     @Override public int getSpanSize(int position) {
-                        return getItemViewType(position) == PROGRESS_TYPE ? 2 : 1;
+                        return getItemViewType(position) == PROGRESS_TYPE ? layoutManager.getSpanCount() : 1;
                     }
                 });
             }
