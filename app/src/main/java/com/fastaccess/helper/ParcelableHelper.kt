60a553feda66fe2137712f5ee86de5366a941819
veyndan <veyndan@gmail.com>
2017-12-30 17:59:55 +0000

    Force Kotlin implementations of Parcelable to enforce the Parcel argument of writeToParcelable to be non null

--- app/src/main/java/com/fastaccess/helper/ParcelableHelper.kt
+++ app/src/main/java/com/fastaccess/helper/ParcelableHelper.kt
@@ -1,7 +1,9 @@
 package com.fastaccess.helper
 
+import android.os.Parcel
 import android.os.Parcelable
 
 interface KParcelable : Parcelable {
     override fun describeContents() = 0
+    override fun writeToParcel(dest: Parcel, flags: Int)
 }
