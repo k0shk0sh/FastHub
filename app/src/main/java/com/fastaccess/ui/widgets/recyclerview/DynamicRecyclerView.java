f14dcc641dc2afd37dc977b3e39007131b262070
Bernardo Ferrari <bernaferrari2@gmail.com>
2018-03-25 16:46:54 -0300

    Fix: getLayoutManager() on DynamicRecyclerView is never GridLayoutManager, since GridLayoutManager is a LinearLayoutManager and LinearLayoutManager is always called before it.

--- app/src/main/java/com/fastaccess/ui/widgets/recyclerview/DynamicRecyclerView.java
+++ app/src/main/java/com/fastaccess/ui/widgets/recyclerview/DynamicRecyclerView.java
@@ -149,10 +149,10 @@ public class DynamicRecyclerView extends RecyclerView {
 
     private boolean canAddDivider() {
         if (getLayoutManager() != null) {
-            if (getLayoutManager() instanceof LinearLayoutManager) {
-                return true;
-            } else if (getLayoutManager() instanceof GridLayoutManager) {
+            if (getLayoutManager() instanceof GridLayoutManager) {
                 return ((GridLayoutManager) getLayoutManager()).getSpanCount() == 1;
+            } else if (getLayoutManager() instanceof LinearLayoutManager) {
+                return true;
             } else if (getLayoutManager() instanceof StaggeredGridLayoutManager) {
                 return ((StaggeredGridLayoutManager) getLayoutManager()).getSpanCount() == 1;
             }
