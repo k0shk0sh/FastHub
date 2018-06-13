2398174ca15405abbc2e5f184f2d243919c156eb
k0shk0sh <kosh20111@gmail.com>
2018-06-13 19:09:35 +0200

    animate fab to end

--- app/src/main/java/com/fastaccess/github/ui/modules/main/fragment/MainFragment.kt
+++ app/src/main/java/com/fastaccess/github/ui/modules/main/fragment/MainFragment.kt
@@ -4,15 +4,35 @@ import android.os.Bundle
 import android.view.View
 import com.fastaccess.github.R
 import com.fastaccess.github.base.BaseFragment
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.bottomappbar.BottomAppBar
 import kotlinx.android.synthetic.main.appbar_center_title_layout.*
+import kotlinx.android.synthetic.main.main_fragment_layout.*
 
 /**
  * Created by Kosh on 12.06.18.
  */
 class MainFragment : BaseFragment() {
     override fun layoutRes(): Int = R.layout.main_fragment_layout
+
     override fun onFragmentCreatedWithUser(view: View, savedInstanceState: Bundle?) {
         toolbarTitle.setText(R.string.app_name)
+        appBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val offset = Math.abs(verticalOffset)
+            if (offset == appBarLayout.totalScrollRange) {
+                bottomBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+            } else {
+                bottomBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+            }
+        })
+
+        bottomBar.inflateMenu(R.menu.main_menu)
+        bottomBar.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.search -> true
+                else -> false
+            }
+        }
     }
 
     companion object {
