29b5efb917208d18fd66da9593e2661d9cd55e70
k0shk0sh <kosh20111@gmail.com>
2019-09-14 12:26:55 +0200

    fix build

--- dagger/src/main/java/com/fastaccess/fasthub/dagger/annotations/ViewModelKey.kt
+++ dagger/src/main/java/com/fastaccess/fasthub/dagger/annotations/ViewModelKey.kt
@@ -12,4 +12,4 @@ import kotlin.reflect.KClass
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 @Retention(AnnotationRetention.RUNTIME)
 @MapKey
-internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
+annotation class ViewModelKey(val value: KClass<out ViewModel>)
