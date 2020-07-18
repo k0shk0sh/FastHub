78007c06e189f412034036468d7874f3f2ec06ae
moko256 <koutaro.mo@gmail.com>
2020-07-19 07:01:25 +0900

    fix: restrict conversion

--- app/src/main/java/com/prettifier/pretty/PrettifyWebView.java
+++ app/src/main/java/com/prettifier/pretty/PrettifyWebView.java
@@ -233,11 +233,19 @@ public class PrettifyWebView extends NestedWebView {
     private void startActivity(@Nullable Uri url) {
         if (url == null) return;
         if (MarkDownProvider.isImage(url.toString())) {
-            Uri rawUrl = url
-                    .buildUpon()
-                    .authority("raw.githubusercontent.com")
-                    .build();
-            CodeViewerActivity.startActivity(getContext(), rawUrl.toString().replace("blob/", ""), url.toString());
+            String rawUrl;
+            if ("github.com".equals(url.getAuthority()) && "blob".equals(url.getPathSegments().get(2))) {
+                rawUrl = url
+                        .buildUpon()
+                        .authority("raw.githubusercontent.com")
+                        .build()
+                        .toString()
+                        .replace("blob/", "");
+            } else {
+                rawUrl = url.toString();
+            }
+
+            CodeViewerActivity.startActivity(getContext(), rawUrl, url.toString());
         } else {
             String lastSegment = url.getEncodedFragment();
             if (lastSegment != null || url.toString().startsWith("#") || url.toString().indexOf('#') != -1) {
